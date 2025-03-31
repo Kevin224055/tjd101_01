025fa3f7cbab59b84bddc754076022846b13b8c9
Kevin224055 <g7z916b@gmail.com>
2025-03-31 22:50:05 +0800

    微調

--- js/indexscorlltop.js
+++ js/indexscorlltop.js
@@ -1,17 +1,36 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 200) {
+// $(document).ready(function() {
+//     $(window).scroll(function() {
+//         if ($(this).scrollTop() > 200) {
+//             $('.gotop').fadeIn();
+//         } else {
+//             $('.gotop').fadeOut();
+//         }
+//     });
+
+//     $('.gotop').click(function(e) {
+//         e.preventDefault();
+//         $('html, body').animate({
+//             scrollTop: 0
+//         }, 500);
+//     });
+// });
+
+
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() >200){
             $('.gotop').fadeIn();
-        } else {
+        }else{
             $('.gotop').fadeOut();
         }
-    });
+    
+})
 
-    $('.gotop').click(function(e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
+$('.gotop').click(function(e){
+e.preventDefault();
+$('html ,body').animate({
+    scrollTop:0
+},500);
+})
 });
-  
