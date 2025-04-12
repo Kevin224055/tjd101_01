e433a442e71021c52cb79cacc2c57d400c72990f
Kevin224055 <g7z916b@gmail.com>
2025-04-13 02:53:15 +0800

    rwd index

--- js/indexscorlltop.js
+++ js/indexscorlltop.js
@@ -34,3 +34,65 @@ $('html ,body').animate({
 },500);
 })
 });
+
+
+
+
+
+
+//   $(function () {
+//     $('.hamburger').click(function () {
+//       $('nav ul').toggleClass('show');
+//     });
+//   });
+
+
+
+
+
+// $(function(){
+//     $('.hamburger').click(function(){
+//         // $(this).addClass('active')
+//         $(this).toggleClass('active')
+//     })
+// })
+
+
+
+$(function () {
+    $('.hamburger').click(function () {
+      $('.transition').toggleClass('show');
+    });
+  });
+
+
+
+
+
+
+  $(document).ready(function() {
+    $('.transition li a').on('click', function(e) {
+        // 檢查螢幕寬度是否 ≤ 820px
+        if (window.matchMedia("(max-width: 820px)").matches) {
+            e.preventDefault(); // 在行動版阻止跳轉
+        }
+
+        var $side = $(this).siblings('.side');
+        
+        // 收起其他子選單
+        $('.side').not($side).removeClass('active').slideUp(300);
+        
+        // 切換當前子選單
+        $side.toggleClass('active').slideToggle(300);
+    });
+    
+});
+
+
+
+
+
+
+
+  
+  
